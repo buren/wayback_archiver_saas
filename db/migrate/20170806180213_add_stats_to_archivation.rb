f6b8de3fd00973a7040e40a0de338c8a2c0154d6
removedporn <86824510+removedporn@users.noreply.github.com>
2021-11-29 20:16:48 +0800

    Delete db directory

--- db/migrate/20170806180213_add_stats_to_archivation.rb
+++
@@ -1,5 +0,0 @@
-class AddStatsToArchivation < ActiveRecord::Migration[5.1]
-  def change
-    add_column :archivations, :stats, :jsonb, null: false, default: '{}'
-  end
-end
--- db/migrate/20170806200018_add_uuid_to_archivations.rb
+++
@@ -1,5 +0,0 @@
-class AddUuidToArchivations < ActiveRecord::Migration[5.1]
-  def change
-    add_column :archivations, :uuid, :string
-  end
-end
